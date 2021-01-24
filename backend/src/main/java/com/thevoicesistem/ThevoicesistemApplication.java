b4274bc3435b85f6962cffc67c8b13dc46f5eed0
AbmaelFerreira <abmael_ninha@hotmail.com>
2021-01-23 23:37:42 -0400

    Iniciando o projeto

---
+++ backend/src/main/java/com/thevoicesistem/ThevoicesistemApplication.java
@@ -0,0 +1,13 @@
+package com.thevoicesistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThevoicesistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThevoicesistemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thevoicesistem/ThevoicesistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thevoicesistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThevoicesistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
