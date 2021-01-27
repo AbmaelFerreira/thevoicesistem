63cfe016bf92ccce3f4398b5d7b7c811b8a5ce35
AbmaelFerreira <abmael_ninha@hotmail.com>
2021-01-27 06:47:11 -0400

    Adicionar novo Album
    Adicionado o serviço que insere um novo album e o recurso que insere um novo album. A uri esta retornando no corpo da mensagem com a novo id do album criado

---
+++ backend/src/main/java/com/thevoicesistem/resources/AlbumResources.java
@@ -0,0 +1,50 @@
+package com.thevoicesistem.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.thevoicesistem.domain.Album;
+import com.thevoicesistem.service.AlbumService;
+
+@RestController
+@RequestMapping(value="/albuns")
+public class AlbumResources {
+	
+	@Autowired
+	private AlbumService albumService;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/{id}")
+	public ResponseEntity<Album> find(@PathVariable Integer id) {
+		
+		Album obj = albumService.find(id); 
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Album>> findAll() {
+		List<Album> lista = albumService.findAll();
+		
+		return ResponseEntity.ok().body(lista);
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert (@RequestBody Album album){
+		album = albumService.insert(album);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(album.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+}
--- backend/src/main/java/com/thevoicesistem/service/AlbumService.java
+++ backend/src/main/java/com/thevoicesistem/service/AlbumService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.thevoicesistem.domain.Album;
+import com.thevoicesistem.domain.Artista;
 import com.thevoicesistem.repository.AlbumRepository;
 
 @Service
@@ -15,8 +19,22 @@ public class AlbumService {
 	@Autowired
 	private AlbumRepository albumRepository;
 	
-	public List<Album> find(){
+	public Album find(Integer id){
+		Optional<Album> obj = albumRepository.findById(id);
+		return obj.orElse(null);
+	}
+	
+	public List<Album> findAll(){
 		List<Album> obj = albumRepository.findAll();
 		return obj;
 	}
+	
+	
+	public Album insert(Album obj) {
+		obj.setId(null);
+		
+		return albumRepository.save(obj);
+	}
+	
+	
 }
